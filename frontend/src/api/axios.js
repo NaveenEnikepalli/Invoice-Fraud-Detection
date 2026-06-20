375c411ade956ca7f50939ae27213850a64cd27a
Your Name <your.email@example.com>
2026-06-20 14:59:59 +0530

    Final Commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://invoice-fraud-detection-fqie.onrender.com/api',
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
